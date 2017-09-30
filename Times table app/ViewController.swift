b701d0774cf8e5d5fb2af84e0b070fd15ab0dc0f
Arkadiusz Stankiewicz <arkadiuszstankiewicz@MacBook-Pro-test.local>
2017-09-30 12:24:24 +0200

    simple program #1

--- Times table app/ViewController.swift
+++ Times table app/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var tableView: UITableView!
+    @IBAction func sliderChanged(_ sender: Any) {
+        
+        tableView.reloadData()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +27,21 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = String((Int(slider.value * 20)) * (indexPath.row + 1))
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate{
+    
 }
 
